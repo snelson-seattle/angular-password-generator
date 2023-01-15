9c0405c5113a3e2322309352f311db74e65aa023
Scott Nelson <snelson@instructors.2u.com>
2023-01-15 13:20:09 -0800

    added event binding to app component for checkboxes and password length input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length = 0;
+  includeLowers = false;
+  includeUppers = false;
+  includeNumbers = false;
+  includeSpecials = false;
+  password = '';
+
   onButtonClick() {
-    console.log('Clicked!');
+    this.password = 'PASSWORD';
+    console.log(this.length);
+  }
+
+  onChangeLength(event: Event ) {
+    const value = (event.target as HTMLInputElement).value;  
+    const parsedInput = parseInt(value);
+
+    if(!isNaN(parsedInput)) {
+      this.length = parsedInput;
+    }else {
+      this.length = 0;
+    }
+  }
+
+  onChangeUseLowercase() {
+    this.includeLowers = !this.includeLowers;
+  }
+
+  onChangeUseUppercase() {
+    this.includeUppers = !this.includeUppers;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSpecials() {
+    this.includeSpecials = !this.includeSpecials;
   }
 }
