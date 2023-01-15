34c8b66b822117d070abea73da139499609b7213
Scott Nelson <snelson@instructors.2u.com>
2023-01-15 15:23:32 -0800

    finished password generation algorithm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,95 @@
 import { Component } from '@angular/core';
 
+const specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const lowercaseLetters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+const uppercaseLetters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+function getRandom(array: string[]) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,17 +104,42 @@ export class AppComponent {
   password = '';
 
   onButtonClick() {
-    this.password = 'PASSWORD';
-    console.log(this.length);
+    this.password = "";
+    let characterSet: string[] = [];
+
+    if(this.length === 0) {
+      return;
+    }
+
+    if (this.includeLowers) {
+      characterSet = characterSet.concat(lowercaseLetters);
+    }
+
+    if(this.includeUppers) {
+      characterSet = characterSet.concat(uppercaseLetters);
+    }
+
+    if(this.includeNumbers) {
+      characterSet = characterSet.concat(numericCharacters);
+    }
+
+    if(this.includeSpecials) {
+      characterSet = characterSet.concat(specialCharacters);
+    }
+
+    for(let i = 0; i < this.length; i++) {
+      this.password += getRandom(characterSet);
+    }
+
   }
 
-  onChangeLength(event: Event ) {
-    const value = (event.target as HTMLInputElement).value;  
+  onChangeLength(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
     const parsedInput = parseInt(value);
 
-    if(!isNaN(parsedInput)) {
+    if (!isNaN(parsedInput)) {
       this.length = parsedInput;
-    }else {
+    } else {
       this.length = 0;
     }
   }
